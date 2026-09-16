af4e9b1297152aa3764a9b7e79086243146d9f83
nguyenhongthai2005 <nguyenhongthai06092005@gmail.com>
2026-09-16 15:52:24 +0700

    feat(service): add ProductService for create and get all products

---
+++ slot3/ProductService/product-service/src/main/java/com/fudn/product_service/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.fudn.product_service.service;
+
+import com.fudn.product_service.dto.ProductRequest;
+import com.fudn.product_service.dto.ProductResponse;
+import com.fudn.product_service.model.Product;
+import com.fudn.product_service.repository.IProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final IProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(product -> ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build()).collect(Collectors.toList());
+    }
+}
