fbf5e547fcad3e73d807fb06237f704f169b524b
nguyenhongthai2005 <nguyenhongthai06092005@gmail.com>
2026-09-18 15:47:11 +0700

    feat(ProductController): implement TODO 3 and 4 PUT and DELETE endpoints

--- slot4/product-service/product-service/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
+++ slot4/product-service/product-service/product-service/src/main/java/com/fudn/product_service/controller/ProductController.java
@@ -51,9 +51,12 @@ public class ProductController {
     //          @PathVariable String id,
     //          @RequestBody ProductRequest productRequest) { ... }
     // ==========================================================
-    // TODO: viết endpoint update tại đây
-
-
+    @PutMapping("/{id}")
+    public ProductResponse updateProduct(
+            @PathVariable String id,
+            @RequestBody ProductRequest productRequest) {
+        return productService.updateProduct(id, productRequest);
+    }
     // ==========================================================
     // TODO 4 — Endpoint DELETE sản phẩm
     // ----------------------------------------------------------
@@ -70,5 +73,9 @@ public class ProductController {
     //   @ResponseStatus(HttpStatus.NO_CONTENT)
     //   public void deleteProduct(@PathVariable String id) { ... }
     // ==========================================================
-    // TODO: viết endpoint delete tại đây
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable String id) {
+        productService.deleteProduct(id);
+    }
 }
