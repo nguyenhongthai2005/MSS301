1ecaa55cd678b53f3f321ea3766af5c0dd35dba1
nguyenhongthai2005 <nguyenhongthai06092005@gmail.com>
2026-09-25 16:48:19 +0700

    test(docx): add document test postman

--- slot6/order-service/src/test/java/com/fudn/orderservice/OrderServiceApplicationTests.java
+++ slot6/order-service/src/test/java/com/fudn/orderservice/OrderServiceApplicationTests.java
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
+import org.wiremock.spring.EnableWireMock;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
+@EnableWireMock
 class OrderServiceApplicationTests {
 
     @ServiceConnection
